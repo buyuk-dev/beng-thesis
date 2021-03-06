14fa19404a06100a7eb0fdf45b867641f931a5a8
Michał Michalski <buyuk.dev@gmail.com>
2021-03-06 22:35:34 +0100

    Connecting to muse with python.

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from muselsl import stream, list_muses
+from pylsl import StreamInlet, resolve_byprop
+
+MAC_ADDRESS = "00:55:DA:B5:7E:45"
+
+## This is synchronous command.
+## How to make it run asynchronously until stop signal?
+# stream(MAC_ADDRESS)
+
+streams = resolve_byprop('type', 'EEG', timeout=2)
+if len(streams) == 0:
+    raise RuntimeError('Failed to start EEG stream.')
+
+inlet = StreamInlet(streams[0], max_chunklen=12)
+eeg_time_correction = inlet.time_correction()
+
+info = inlet.info()
+description = info.desc()
+
+sampling = int(info.nominal_srate())
+
+print(info.as_xml())
+
+
