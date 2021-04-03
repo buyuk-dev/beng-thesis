2c24dfbd2d3d209ea59d180e643c33db0e942582
Michał Michalski <michal@buyuk-dev.com>
2021-04-03 23:52:35 +0200

    Dynamically calculated number of channels to plot;

--- app/main.py
+++ app/main.py
@@ -2,81 +2,93 @@ from muselsl import stream, list_muses
 from pylsl import StreamInlet, resolve_byprop
 import threading
 import pprint
+import time
 
 import matplotlib.pyplot as pyplot
 from matplotlib.animation import FuncAnimation
 
-MAC_ADDRESS = "00:55:DA:B5:7E:45"
+import configuration
 
 
-def start_stream():
-    """
-    """
-    stream(MAC_ADDRESS)
+class MuseEegStream:
+
+    def __init__(self, muse_address=configuration.MAC_ADDRESS):
+        self.muse_address = muse_address
+        self.data_lock = threading.Lock()
 
 
-stream_thread = threading.Thread(target=start_stream)
-stream_thread.start()
+    def connect(self):
+        """ Setup Muse LSL connection.
+        """
+        def stream_thread():
+            stream(self.muse_address, ppg_enabled=True, acc_enabled=True, gyro_enabled=True)
+
+        self.stream_thread = threading.Thread(target=stream_thread)
+        self.stream_thread.start()
+        time.sleep(5.0)
+        print("LSL stream started.")
+
+        self.streams = resolve_byprop('type', 'EEG', timeout=2)
+        if len(self.streams) == 0:
+            raise RuntimeError('Failed to start EEG stream.')
+
+        print("Streams resolved.")
+
+        self.inlet = StreamInlet(self.streams[0], max_chunklen=30)
+
+        print("Inlet created.")
+
+        self.sampling = int(self.inlet.info().nominal_srate())
+        self.nchannels = self.inlet.info().channel_count()
+
+        print(f"Stream sampling rate {self.sampling},") 
+        print(f"Stream number of channels {self.nchannels}.")
+
+        print(self.inlet.info().as_xml())
+
+        self.window_size = self.sampling * 5
+        self.data = [tuple([0] * self.nchannels)] * self.window_size
+
+    def start(self):
+        """ Start reading samples.
+        """
+        def collect_data():
+            while True:
+                new_data, timestamps = self.inlet.pull_chunk(timeout=0.1)
+                print(new_data[0])
+                with self.data_lock:
+                    self.data.extend(new_data)
+                    self.data = self.data[-self.window_size:]
+       
+        self.collect_thread = threading.Thread(target=collect_data)
+        self.collect_thread.start()
 
 
-streams = resolve_byprop('type', 'EEG', timeout=2)
-if len(streams) == 0:
-    raise RuntimeError('Failed to start EEG stream.')
+if __name__ == '__main__':
 
+    muse_stream = MuseEegStream()
+    muse_stream.connect()
+    muse_stream.start()
 
-inlet = StreamInlet(streams[0], max_chunklen=12)
-eeg_time_correction = inlet.time_correction()
-info = inlet.info()
-description = info.desc()
+    fig = pyplot.figure()
+    axs = [fig.add_subplot(muse_stream.nchannels, 1, i+1) for i in range(muse_stream.nchannels)]
 
+    def draw(event):
+        for ax in axs:
+            ax.clear()
 
-data = [(0,0,0,0)] * 1024
-data_lock = threading.Lock()
+        with muse_stream.data_lock:
+            [ax.set_ylim([-200,200]) for ax in axs]
 
+            data_channel = [
+                [d[i] for d in muse_stream.data]
+                for i in range(muse_stream.nchannels)
+            ]
 
-def collect_data():
-    global data
-    chunk, timestamps = inlet.pull_chunk(timeout=0.1)
-    data = list(chunk)
+            for ax, channel in zip(axs, data_channel):
+                ax.plot(channel)
 
-    while True:
-        new_data, timestamps = inlet.pull_chunk(timeout=0.1)
-        with data_lock:
-            data.extend(new_data)
-
-
-fig = pyplot.figure()
-axs = [fig.add_subplot(411), fig.add_subplot(412), fig.add_subplot(413), fig.add_subplot(414)]
-
-def draw(event):
-    global data
-    global axs
-
-    for ax in axs:
-        ax.clear()
-
-    with data_lock:
-        data = data[-1024:]
-        data_0 = [d[0] for d in data]
-        data_1 = [d[1] for d in data]
-        data_2 = [d[2] for d in data]
-        data_3 = [d[3] for d in data]
-
-        [ax.set_ylim([-200,200]) for ax in axs]
-
-        axs[0].plot(data_0)
-        axs[1].plot(data_1)
-        axs[2].plot(data_2)
-        axs[3].plot(data_3)
-
-
-collect_thread = threading.Thread(target=collect_data)
-collect_thread.start()
-
-ani = FuncAnimation(fig, draw, interval=100)
-pyplot.show()
-
-
-sampling = int(info.nominal_srate())
-print(info.as_xml())
+            
+    ani = FuncAnimation(fig, draw, interval=100)
+    pyplot.show()
 
