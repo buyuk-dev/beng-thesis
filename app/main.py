b168455d86efcd0f33b78a93af19020731170aa4
Michał Michalski <michal@buyuk-dev.com>
2021-04-03 21:14:20 +0200

    Plotting all channels in real time using matplotlib;

--- app/main.py
+++ app/main.py
@@ -19,55 +19,64 @@ stream_thread = threading.Thread(target=start_stream)
 stream_thread.start()
 
 
-
-
 streams = resolve_byprop('type', 'EEG', timeout=2)
 if len(streams) == 0:
     raise RuntimeError('Failed to start EEG stream.')
 
 
-
-
-
 inlet = StreamInlet(streams[0], max_chunklen=12)
 eeg_time_correction = inlet.time_correction()
-
-
 info = inlet.info()
 description = info.desc()
 
 
-data = []
+data = [(0,0,0,0)] * 1024
+data_lock = threading.Lock()
+
+
+def collect_data():
+    global data
+    chunk, timestamps = inlet.pull_chunk(timeout=0.1)
+    data = list(chunk)
+
+    while True:
+        new_data, timestamps = inlet.pull_chunk(timeout=0.1)
+        with data_lock:
+            data.extend(new_data)
 
 
 fig = pyplot.figure()
-ax = fig.add_subplot(111)
-
+axs = [fig.add_subplot(411), fig.add_subplot(412), fig.add_subplot(413), fig.add_subplot(414)]
 
 def draw(event):
     global data
-    global ax
-    ax.clear()
-    ax.plot(data[-200:])
-    data = data[-400:]
+    global axs
 
+    for ax in axs:
+        ax.clear()
+
+    with data_lock:
+        data = data[-1024:]
+        data_0 = [d[0] for d in data]
+        data_1 = [d[1] for d in data]
+        data_2 = [d[2] for d in data]
+        data_3 = [d[3] for d in data]
+
+        [ax.set_ylim([-200,200]) for ax in axs]
+
+        axs[0].plot(data_0)
+        axs[1].plot(data_1)
+        axs[2].plot(data_2)
+        axs[3].plot(data_3)
+
+
+collect_thread = threading.Thread(target=collect_data)
+collect_thread.start()
 
 ani = FuncAnimation(fig, draw, interval=100)
-
-print("shwoing plot")
-#pyplot.show()
-
-chunk, timestamps = inlet.pull_chunk(timeout=1.0)
-data = list(chunk)
-
-while True:
-    new_data, timestamps = inlet.pull_chunk()
-    data.extend(new_data)
-
-
+pyplot.show()
 
 
 sampling = int(info.nominal_srate())
 print(info.as_xml())
-
 
