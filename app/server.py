e5e60f091f0aac0cfe16ad5635047205cc3faa58
Michal Michalski <michal@buyuk-dev.com>
2021-04-06 23:49:12 +0200

    Implement /spotify/connect handler;

--- app/server.py
+++ app/server.py
@@ -1,8 +1,12 @@
 import flask
 import threading
 import time
+import webbrowser
 
 from logger import logger
+import configuration
+import spotify.api
+import spotify.filters
 
 
 server = flask.Flask("EegDataCollectionServer")
@@ -12,8 +16,37 @@ server.debug = False
 @server.route("/callback")
 def spotify_auth_callback():
     logger.info("Spotify auth callback has been triggered.")
-    code = flask.request.args.get("code", None)
-    logger.info(f"Spotify authorization code: {code}")
+    auth_code = flask.request.args.get("code", None)
+    if auth_code == None:
+        logger.error(f"Spotify auth callback triggered with no auth code.")
+        return flask.make_response("Error: Auth code is None.", 401)
+
+    logger.debug(f"Auth code is {auth_code}")
+    code, resp = spotify.api.request_token(auth_code, configuration.SPOTIFY_CALLBACK_URL)
+    if code != 200:
+        logger.error(f"Failed to retrieve access token: HTTP {code}.")
+        return flask.make_response(resp, code)
+    token = resp["access_token"]
+
+    code, resp = spotify.api.get_user_profile(token)
+    if code != 200:
+        logger.error(f"Failed to retireve user profile info: HTTP {code}.")
+        return flask.make_response(resp, code)
+    user_id = resp["id"]
+
+    code, resp = spotify.api.get_user_playlists(token, user_id)
+    if code != 200:
+        logger.error(f"Failed to retrieve user's playlists: HTTP {code}.")
+        return flask.make_response(resp, code)
+
+    configuration.PLAYLISTS = spotify.filters.playlists(resp)
+    configuration.USER_ID = user_id
+    configuration.TOKEN = token
+
+    logger.debug(f"token = {configuration.TOKEN}")
+    logger.debug(f"user_id = {configuration.USER_ID}")
+    logger.debug(f"playlists = {configuration.PLAYLISTS}")
+
     return flask.make_response("OK", 200)
 
 
@@ -26,6 +59,8 @@ def on_mark_song_command(value):
 @server.route("/spotify/connect")
 def on_spotify_connect():
     logger.info("Connecting to Spotify...")
+    auth_url = spotify.api.authorize_user(configuration.SPOTIFY_CALLBACK_URL)
+    webbrowser.open(auth_url)
     return flask.make_response("OK", 200)
 
 
