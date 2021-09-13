a2f62595437b008b043db4fc47dc1ac19deeeca7
Michał Michalski <michal@buyuk-dev.com>
2021-09-13 16:36:24 +0200

    [client] Make it easier to define new commands;

--- client/cli.py
+++ client/cli.py
@@ -2,42 +2,99 @@
 """
 
 import argparse
-import pprint
+from pprint import pprint
 
 from logger import logger
 import client
 
 
-class ConfigCommand:
-    @staticmethod
-    def on_config_request(args):
-        userid = 0
-        status, config = client.get_config(userid)
-        pprint.pprint(config)
+class BaseCommandParser:
+
+    def __init__(self, parent=None, command=None):
+        """ If no parent is specified, creates highest level root parser.
+            Otherwise creates a subcommand parser, for which command argument is required.
+        """
+        if parent is None:
+            self.parser = argparse.ArgumentParser()
+        else:
+            if type(command) is not str:
+                raise TypeError("Command argument for non-root parsers must be a string.")
+            self.parser = parent.subparsers.add_parser(command)
+
+        self.parent = parent
+        self.subparsers = self.parser.add_subparsers()
+        self.commands = dict()
+
+    def define_command(self, name, callback):
+        """ Defines a command recognized by the parser.
+            Running parser with `<parser> command [command args]` syntax will trigger callback.
+        """
+        if name in self.commands:
+            logger.warning(f"Overriding command parser for: {name}.")
+
+        self.commands[name] = self.subparsers.add_parser(name)
+        self.commands[name].set_defaults(command=callback)
+
+    def add_command_argument(self, name, *args, **kwargs):
+        """ Subcommands that take arguments require specification of those arguments.
+        """
+        self.commands[name].add_argument(*args, **kwargs)
+
+    def run(self):
+        """ Parse arguments and trigger command handler. """
+        args = self.parser.parse_args()
+        args.command(args)
+
+
+class ConfigCommandParser(BaseCommandParser):
+
+    def __init__(self, parent):
+        super().__init__(command="config", parent=parent)
+        self.define_command("show", self.on_show)
+        self.define_command("update", self.on_update)
 
     @staticmethod
-    def on_config_update(args):
+    def on_show(args):
+        pprint(client.get_config(0))
+
+    @staticmethod
+    def on_update(args):
         raise NotImplementedError()
 
 
-class SpotifyCommand:
+class SpotifyCommandParser(BaseCommandParser):
+
+    def __init__(self, parent):
+        super().__init__(command="spotify", parent=parent)
+        self.define_command("connect", self.on_connect)
+        self.define_command("playback", self.on_playback)
+        self.define_command("status", self.on_status)
+
     @staticmethod
     def on_connect(args):
-        print(client.connect_spotify())
+        pprint(client.connect_spotify())
 
     @staticmethod
-    def on_playback_request(args):
-        print(client.get_current_playback())
+    def on_playback(args):
+        pprint(client.get_current_playback())
 
     @staticmethod
-    def on_status_request(args):
-        print(client.spotify_status())
+    def on_status(args):
+        pprint(client.spotify_status())
 
 
-class MuseCommand:
+class MuseCommandParser(BaseCommandParser):
+
+    def __init__(self, parent):
+        super().__init__(command="muse", parent=parent)
+        self.define_command("connect", self.on_connect)
+        self.define_command("disconnect", self.on_disconnect)
+        self.define_command("start", self.on_start)
+        self.define_command("stop", self.on_stop)
+
     @staticmethod
     def on_connect(args):
-        print(client.connect_muse("address"))
+        print(client.connect_muse())
 
     @staticmethod
     def on_disconnect(args):
@@ -52,7 +109,15 @@ class MuseCommand:
         print(client.stop_muse_data_collection())
 
 
-class SessionCommand:
+class SessionCommandParser(BaseCommandParser):
+
+    def __init__(self, parent, config):
+        super().__init__(command="session", parent=parent)
+        self.define_command("start", self.on_start)
+        self.define_command("stop", self.on_stop)
+        self.define_command("label", self.on_label)
+        self.add_command_argument("label", "label", choices=config["labels"])
+
     @staticmethod
     def on_start(args):
         print(client.session_start())
@@ -68,65 +133,13 @@ class SessionCommand:
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(prog="bci")
-    subparsers = parser.add_subparsers()
-
-    userid = 0
-    status, config = client.get_config(userid)
+    status, config = client.get_config(0)
     if "labels" not in config:
         logger.error("Config returned by the server doesn't contain required entry.")
 
-    # Config Commands Parser
-    config_parser = subparsers.add_parser("config")
-    config_subparsers = config_parser.add_subparsers()
-
-    config_show_parser = config_subparsers.add_parser("show")
-    config_show_parser.set_defaults(command=ConfigCommand.on_config_request)
-
-    config_update_parser = config_subparsers.add_parser("update")
-    config_update_parser.set_defaults(command=ConfigCommand.on_config_update)
-
-    # Spotify Commands Parser
-    spotify_parser = subparsers.add_parser("spotify")
-    spotify_subparsers = spotify_parser.add_subparsers()
-
-    spotify_connect_parser = spotify_subparsers.add_parser("connect")
-    spotify_connect_parser.set_defaults(command=SpotifyCommand.on_connect)
-
-    spotify_playback_parser = spotify_subparsers.add_parser("playback")
-    spotify_playback_parser.set_defaults(command=SpotifyCommand.on_playback_request)
-
-    # Muse Commands Parser
-    muse_parser = subparsers.add_parser("muse")
-    muse_subparsers = muse_parser.add_subparsers()
-
-    muse_connect_parser = muse_subparsers.add_parser("connect")
-    muse_connect_parser.set_defaults(command=MuseCommand.on_connect)
-
-    muse_connect_parser = muse_subparsers.add_parser("disconnect")
-    muse_connect_parser.set_defaults(command=MuseCommand.on_disconnect)
-
-    muse_start_parser = muse_subparsers.add_parser("start")
-    muse_start_parser.set_defaults(command=MuseCommand.on_start)
-
-    muse_stop_parser = muse_subparsers.add_parser("stop")
-    muse_stop_parser.set_defaults(command=MuseCommand.on_stop)
-
-    # Session Commands Parser
-    session_parser = subparsers.add_parser("session")
-    session_subparsers = session_parser.add_subparsers()
-
-    session_start_parser = session_subparsers.add_parser("start")
-    session_start_parser.set_defaults(command=SessionCommand.on_start)
-
-    session_stop_parser = session_subparsers.add_parser("stop")
-    session_stop_parser.set_defaults(command=SessionCommand.on_stop)
-
-    # Add label command to the session parser.
-    session_add_label_parser = session_subparsers.add_parser("label")
-    session_add_label_parser.set_defaults(command=SessionCommand.on_label)
-    session_add_label_parser.add_argument("label", choices=config["labels"])
-
-    # Parse Commands
-    args = parser.parse_args()
-    args.command(args)
+    parser = BaseCommandParser()
+    config_cmd_parser = ConfigCommandParser(parser)
+    spotify_cmd_parser = SpotifyCommandParser(parser)
+    muse_cmd_parser = MuseCommandParser(parser)
+    session_cmd_parser = SessionCommandParser(parser, config)
+    parser.run()
